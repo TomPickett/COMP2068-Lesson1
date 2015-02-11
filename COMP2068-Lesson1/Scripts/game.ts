a069bb8bc4b77086ea15f5338a5d8275fe7ce6e6
TomPickett <Pickett.tom7@gmail.com>
2015-02-11 18:27:52 -0500

    Added basic structure for typescript

---
+++ COMP2068-Lesson1/Scripts/game.ts
@@ -0,0 +1,13 @@
+﻿class Player {
+    strength: number;
+    constructor() {
+        this.strength = 10;
+    }
+    jump() {
+        console.debug("you jumped");
+    }
+}
+function main() {
+    var tom = new Player();
+    tom.jump();
+}
